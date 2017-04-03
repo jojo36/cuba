67828c07bdc7bf33682f1ac0f18190564cfd1326
jojo36 <jbodan@orange.fr>
2017-04-03 12:09:21 +0200

    Mise à jour 02/04

---
+++ client/views/js/front/frontRegister.js
@@ -0,0 +1,37 @@
+Template.frontRegister.events({
+
+  'submit form': function(e, template){
+
+    e.preventDefault();
+
+
+    var username = $('input[name="username"]').val();
+    var email = $('input[name="email"]').val();
+    var password = $('input[name="password"]').val();
+    var name = $('input[name="name"]').val();
+    var surname = $('input[name="surname"]').val();
+    var password2 = $('input[name="password2"]').val();
+
+    var user = {
+			username: username,
+			email: email,
+			password: password,
+			profile: {
+        name: name,
+        surName: surname
+      }
+		};
+
+    Accounts.createUser(user, function(err){
+
+      if(err){
+        alert(err.reason);
+      }else {
+        Router.go('backDashboard');
+      }
+
+    });
+
+  }
+
+});
